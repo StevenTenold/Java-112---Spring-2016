e571a3d6a27f78ec06889545ba79a0090bc71a92
StevenTenold <steve.tenold@yahoo.com>
2016-02-29 07:22:23 -0800

    Added files via upload

---
+++ Week3Labs.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+public class Week3Labs 
+{
+/*  // Lab 1 + 2
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("My name is Steven Tenold");
+    }
+    // No Lab 3
+    
+    // Lab 4
+    public static void main (String[] args)
+    {
+        String Hello = "Hello\n";
+        String myName = "My name is Steve Tenold";
+        
+        System.out.printf(Hello + myName);
+         }
+
+    
+    // Lab 5
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int digit1, digit2;
+        
+        System.out.println("Enter the first integer: ");
+        digit1 = input.nextInt();
+        
+        System.out.println("Enter the second integer: ");
+        digit2 = input.nextInt();
+        
+        System.out.printf("You have entered %d and %d%n", digit1, digit2);
+    }
+  
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        int digit1;
+        int digit2;
+        int Addition;
+        int Subtraction;
+        int Division;
+        int Multiplication;
+        
+        System.out.print("Enter the first integer: ");
+        digit1 = input.nextInt();
+        
+        System.out.print("Enter the second integer: ");
+        digit2 = input.nextInt();
+        
+        Addition = digit1 + digit2;
+        Subtraction = digit1 - digit2;
+        Division = digit1 / digit2;
+        Multiplication = digit1 * digit2;
+        
+        System.out.printf("%d + %d = %d%n", digit1, digit2, Addition);
+        System.out.printf("%d - %d = %d%n", digit1, digit2, Subtraction);
+        System.out.printf("%d / %d = %d%n", digit1, digit2, Division);
+        System.out.printf("%d * %d = %d%n", digit1, digit2, Multiplication);
+    }
+*/    
+}
