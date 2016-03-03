a349024f981315a1520831235afe8818dcd9030b
StevenTenold <steve.tenold@yahoo.com>
2016-03-02 20:12:57 -0800

    Added files via upload

---
+++ HungryFlowChart.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class HungryFlowChart 
+{
+   public static void main(String[] args) 
+   {
+       System.out.println("Hungry");
+       System.out.println("Get in Line");
+       System.out.println("Buy Lunch");
+       System.out.println("Eat Lunch");
+       System.out.println("Return Tray");
+       System.out.println("Leave");
+   }
+}
