94d965b64bc0dd401006e151127606c8a5c640f9
StevenTenold <steve.tenold@yahoo.com>
2016-03-16 19:56:21 -0700

    Added files via upload
    Week 6 Lab 2

---
+++ Week6Lab2.java
@@ -0,0 +1,36 @@
+
+package week6labs;
+import java.lang.Math;
+import java.util.Scanner;
+public class Week6Labs 
+{
+
+  
+    public static void main(String[] args) 
+    {
+        int pRandomNumber = 1 + (int)(Math.random() * 10);
+        boolean GoodGuess = false;
+        
+        System.out.println("Guess the Random Number!");
+        while (GoodGuess == false)       
+        {
+            Scanner UserInput = new Scanner(System.in);
+            System.out.println("\nEnter your guess: ");
+            int lUserGuess = UserInput.nextInt();
+            
+            if (lUserGuess == pRandomNumber)
+            {
+                System.out.println("\nThat's Right! You Win!");
+                GoodGuess = true;
+            }
+            
+            else if (lUserGuess > pRandomNumber)
+                System.out.println("Too High. Guess Again.");
+            
+            else if (lUserGuess < pRandomNumber)
+                System.out.println("Too Low. Guess Again.");
+               
+        } 
+    }
+    
+}
