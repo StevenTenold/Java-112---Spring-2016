b33dd06a55035989d244ec13563c1b6b1f897c0a
StevenTenold <steve.tenold@yahoo.com>
2016-03-23 21:57:21 -0700

    Added files via upload

---
+++ Lab_8.java
@@ -0,0 +1,20 @@
+package Week7_Labs;
+
+import java.util.Arrays;
+
+public class Lab_8 
+{
+
+	public static void main(String[] args)
+	{
+		int [] Lab8Array = new int [] {123, 456, 479, 135, 246, 135};
+		
+		Arrays.sort(Lab8Array);
+		//Arrays.sort(Lab8Array);
+		int i;
+
+		for (i = 0; i < 6; i++)
+			System.out.println( Lab8Array[i]);
+
+	}
+}
