e201c198c2e576b429dfcd24d9ac9cfb67e9ece9
StevenTenold <steve.tenold@yahoo.com>
2016-03-23 20:38:54 -0700

    Added files via upload

---
+++ Lab_2.java
@@ -0,0 +1,45 @@
+package Week7_Labs;
+
+import java.lang.Math;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Lab_2 
+{
+
+
+
+    public static void main(String[] args)
+    {
+
+    // CPU & User variables for their respective choices
+            String computer = "";
+            String user = "";
+
+    // STEP #1 - Determining the CPU's Choice - Rock, Paper, Or Scissors      
+            int cpu = 1 + (int)((Math.random() * 10) / 3);
+            
+            if (cpu == 1)
+            	computer = "R";
+            else if (cpu == 2)
+            	computer = "P";
+            else
+            	computer = "S";
+
+
+    // Step #2 - Determining the User's Choice - Rock, Paper, Or Scissors
+            user = JOptionPane.showInputDialog("Enter \"R\", \"P\", or \"S\": ");
+            user.toUpperCase();
+            
+            
+    // Step #3 - // Playing the Game - User vs Opponent
+            if (user == computer)
+            	JOptionPane.showMessageDialog( null, "Tie Game");
+            
+            else if ((computer=="R" && user=="S") || (computer=="P" && user=="R") || (computer == "S" && user == "P"))
+            	JOptionPane.showMessageDialog( null, "Computer Wins");
+            
+            else
+            	JOptionPane.showMessageDialog( null, "User Wins");
+    }
+}
